ee36f34ac22b35211d5c3566375fa605aff66ac0
Vasyl Khvostyk <vasyl.khvostyk@gmail.com>
2024-11-28 09:55:09 +0100

    fix: API key moved to secrets

--- src/api/fetch-weather.ts
+++ src/api/fetch-weather.ts
@@ -1,9 +1,14 @@
 import WeatherData from '../types/weather-data';
 
-const API_KEY = '9b074ad956134784824ad2e8ec708427';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchWeather = async (city: string): Promise<WeatherData> => {
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  if (!API_KEY) {
+    throw new Error('Weather API key not found');
+  }
+
   const response = await fetch(
     `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_APP_BASE: string;
+  readonly VITE_WEATHER_API_KEY: string;
 }
 
 interface ImportMeta {
